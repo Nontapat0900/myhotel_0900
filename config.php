865fb79310d064b08affeb6bed967558e1dde946
Nontapat0900 <nontapatHAM@gmail.com>
2026-02-04 11:34:35 +0700

    commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Connect to server and select database.
+define('servername', 'localhost');
+define('username', 'root');
+define('password', '');
+define('dbname', 'myhotel_0900');
+
+$objCon = mysqli_connect(servername, username,password,dbname);
+
+?>
+
